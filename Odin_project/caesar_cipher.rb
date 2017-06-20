8d415039f5b87b97a6712723b47b8960ae970a96
tanja4r <tatjana.raznatovic@gmail.com>
2017-06-20 16:32:34 +0200

    Caesar chipher

--- Odin_project/caesar_cipher.rb
+++ Odin_project/caesar_cipher.rb
@@ -1,7 +1,19 @@
-def ceaser_cipher(phrase, shift)
+def caeser_cipher(phrase, shift)
   ph = phrase.split
   ph.each do |word|
+    char = word.split("")
+    char.each do |i|
+      a = word[i].ord
+      puts a
+      if (a >= 65 && a <= 90 && a+shift <90) || (a >= 97 && a <= 122 && a+shift <122)
+        word[i] = (word[i].ord+shift).chr
+      elsif (a >= 65 && a <= 90 && a+shift >90) || (a >= 97 && a <= 122 && a+shift > 122)
+        word[i] = (word[i].ord+shift-26).chr
+      end
+    end
     puts word
   end
-  ph.join(" ")
+  puts ph.join(" ")
 end
+
+caeser_cipher("What a string!", 5)
